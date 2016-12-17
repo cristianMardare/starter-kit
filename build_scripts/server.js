b855895eebcda5dba3ddd75ae31aac175bc750fa
Cristian Mardare <cristian.mardare.dev@gmail.com>
2016-12-17 19:17:54 +0200

    Added bundling using Webpack

--- build_scripts/server.js
+++ build_scripts/server.js
@@ -1,8 +1,18 @@
 import express from 'express'
 import  path from 'path'
+import webpack from 'webpack'
+import config from '../webpack.config.dev'
+
+import webpackMW from 'webpack-dev-middleware'
 
 const port = 62226;
 const app = new express();
+const compiler = webpack(config);
+
+app.use(webpackMW(compiler, {
+	noInfo: true,
+	publickPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res){
 			res.sendFile(path.join(__dirname, '../src/index.html'));
